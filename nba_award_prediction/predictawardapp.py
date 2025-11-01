f303d0f011b098c7e6aca2e1237e4a1c96ebace8
Ibrahim Eksi <95856789+ibraeksi@users.noreply.github.com>
2025-11-01 23:51:03 +0100

    added all-star prediction

--- nba_award_prediction/predictawardapp.py
+++ nba_award_prediction/predictawardapp.py
@@ -4,9 +4,12 @@ import pickle
 from pathlib import Path
 
 trained_model = Path(__file__).parents[0] / 'models/undersampling_random_forest_v03.pkl'
+trained_allstar_model = Path(__file__).parents[0] / 'models/allstarselection_random_forest_v01.pkl'
+player_bio_data = Path(__file__).parents[0] / 'data/raw/nba_historical_player_bio.csv'
 
 from modules.get_league_standings import get_league_standings
 from modules.get_curr_mvp_table import get_curr_mvp_table
+from modules.get_curr_allstars import get_curr_allstars
 
 st.set_page_config(
     page_title="NBA Award Predictions",
@@ -16,7 +19,7 @@ st.set_page_config(
 
 st.subheader("2025-26 NBA Regular Season Award Predictions")
 
-tab1, tab2 = st.tabs([":chart_with_upwards_trend: Standings", ":crown: MVP Tracker"])
+tab1, tab2, tab3, tab4 = st.tabs([":chart_with_upwards_trend: Standings", ":crown: MVP Tracker", ":star: West All-Stars", ":star: East All-Stars"])
 
 with tab1:
     left_sta, gap_sta, right_sta = st.columns([5.5, 1, 5.5], vertical_alignment="top")
@@ -39,3 +42,29 @@ with tab2:
     mvptable = get_curr_mvp_table(model)
     st.dataframe(mvptable, hide_index=True)
     st.session_state["mvptable"] = mvptable
+
+with tab3:
+    bio = pd.read_csv(player_bio_data)
+    st.session_state["player_bio"] = bio
+
+    with open(trained_allstar_model, "rb") as allstar_model_file:
+        allstar_model = pickle.load(allstar_model_file)
+
+    weststarters, eaststarters, westbench, eastbench = get_curr_allstars(allstar_model, bio)
+
+    st.markdown("Western Conference Starters")
+    st.dataframe(weststarters, hide_index=True)
+    st.session_state["weststarters"] = weststarters
+
+    st.markdown("Western Conference Bench")
+    st.dataframe(westbench, hide_index=True)
+    st.session_state["westbench"] = westbench
+
+with tab4:
+    st.markdown("Eastern Conference Starters")
+    st.dataframe(eaststarters, hide_index=True)
+    st.session_state["eaststarters"] = eaststarters
+
+    st.markdown("Eastern Conference Bench")
+    st.dataframe(eastbench, hide_index=True)
+    st.session_state["eastbench"] = eastbench
