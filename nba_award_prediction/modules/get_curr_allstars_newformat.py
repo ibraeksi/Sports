55189000a931c7ca83ee065e4156ea9fc0b9b271
Ibrahim Eksi <95856789+ibraeksi@users.noreply.github.com>
2025-11-12 20:42:34 +0100

    added new format all-star predictions

---
+++ nba_award_prediction/modules/get_curr_allstars_newformat.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import numpy as np
+from nba_api.stats.endpoints import leaguestandings
+from nba_api.stats.endpoints import leaguedashplayerstats
+
+
+def get_curr_allstars_newformat(model, bio):
+    """
+    Retrieves current season stats using nba_api and returns a table
+    with all-stars according to the new format
+    model = Trained machine learning model to predict all-stars
+    bio = Player bios for adding position and country
+    """
+    # Get current stats
+    pl = leaguedashplayerstats.LeagueDashPlayerStats(season='2025-26').get_data_frames()[0]
+    tm = leaguestandings.LeagueStandings(season='2025-26').get_data_frames()[0][['TeamID', 'TeamName', 'Conference', 'WINS', 'LOSSES', 'WinPCT', 'PlayoffRank']]
+
+    # Merge stats for prediction
+    pl = pl.dropna(subset=['PLAYER_ID']).reset_index(drop=True)
+    df = pd.merge(pl, tm, left_on=['TEAM_ID'], right_on=['TeamID'], how='left')
+
+    posdict = dict(bio[['PERSON_ID', 'POSITION']].values)
+    countrydict = dict(bio[['PERSON_ID', 'COUNTRY']].values)
+    df['POS'] = df['PLAYER_ID'].map(posdict)
+    df['COUNTRY'] = df['PLAYER_ID'].map(countrydict)
+    df['CONF'] = df['Conference'].map({'West':'W', 'East':'E'})
+
+    # Filter out non MVP level players
+    df['GP_PCT'] = df['GP'] / (df['WINS'] + df['LOSSES'])
+    df.loc[df['GP_PCT'] > 1, 'GP_PCT'] = 1
+
+    df['MPG'] = df['MIN']/df['GP']
+    df['PPG'] = df['PTS']/df['GP']
+
+    df.drop(df.loc[df['MPG'] < 24].index, inplace=True)
+    df.drop(df.loc[df['GP_PCT'] < 0.4].index, inplace=True)
+
+    df = df.reset_index(drop=True)
+    df[['RPG', 'APG', 'TPG', 'SPG', 'BPG', 'FPG', 'PMPG']] = df[['REB', 'AST', 'TOV', 'STL', 'BLK', 'NBA_FANTASY_PTS', 'PLUS_MINUS']].div(df['GP'], axis=0)
+
+    conf_type = ['E', 'W']
+
+    for conf in conf_type:
+        nba_group = df[df['CONF'] == conf]
+        df['PTS_' + conf] = nba_group['PPG'].rank(pct = True)
+        df['FP_' + conf] = nba_group['FPG'].rank(pct = True)
+        df['W_PCT_' + conf] = nba_group['WinPCT'].rank(pct = True)
+
+    df = df.fillna(0)
+    nbanowmod = df[['PLAYER_NAME', 'COUNTRY', 'CONF', 'GP_PCT', 'POS', 'PTS_E', 'FP_E',
+                    'W_PCT_E', 'PTS_W', 'FP_W', 'W_PCT_W']].rename(columns={'PLAYER_NAME':'PLAYER'})
+
+    # Predict All-Stars
+    cols = nbanowmod.columns
+    test_cols = cols.drop(['GP_PCT', 'COUNTRY', 'PLAYER', 'POS', 'CONF'])
+
+    new_predictions = model.predict(nbanowmod[test_cols])
+    proba_predict = model.predict_proba(nbanowmod[test_cols])
+
+    nbanowmod['PREDICT'] = new_predictions
+    nbanowmod[['PREDICT_NOT', 'PREDICT_E', 'PREDICT_W']] = pd.DataFrame(proba_predict)
+    nbanowmod['PREDICT_PROB'] = 100*(1-nbanowmod['PREDICT_NOT'])
+    allstarprob = dict(nbanowmod[['PLAYER','PREDICT_PROB']].values)
+    df['PREDICT_PROB'] = df['PLAYER_NAME'].map(allstarprob)
+
+    finaldf = df[['PREDICT_PROB', 'POS', 'PLAYER_NAME', 'COUNTRY', 'AGE', 'GP', 'TeamName', 'W', 'L',
+                        'Conference', 'PlayoffRank', 'WinPCT', 'WINS', 'LOSSES',
+                        'GP_PCT', 'MPG', 'PPG', 'RPG', 'APG', 'TPG', 'SPG', 'BPG']].rename(
+                            columns={'PLAYER_NAME':'PLAYER', 'TeamName':'TEAM', 'Conference':'CONF',
+                                    'Record':'RECORD', 'WinPCT':'WIN%', 'PlayoffRank':'TEAMRANK',
+                                    'MPG':'MIN', 'PPG':'PTS', 'RPG':'REB', 'APG':'AST', 'TPG':'TOV',
+                                    'SPG':'STL', 'BPG':'BLK', 'PREDICT_PROB':'PROB%', 'GP_PCT':'GP%'})
+
+    finaldf['AGE'] = finaldf['AGE'].astype(int)
+    finaldf['TEAMRANK'] = '#'+finaldf['TEAMRANK'].astype(str)
+    finaldf[['WIN%', 'MIN', 'PTS', 'REB', 'AST', 'TOV', 'STL', 'BLK', 'PROB%', 'GP%']] = finaldf[['WIN%', 'MIN', 'PTS', 'REB', 'AST', 'TOV', 'STL', 'BLK', 'PROB%', 'GP%']].round(2)
+
+    finaldf['GP%'] = np.round(100*finaldf['GP%'],2)
+    finaldf['RANK'] = finaldf['CONF'] + ' ' + finaldf['TEAMRANK']
+    finaldf['RECORD'] = finaldf['WINS'].astype(str) + '-' + finaldf['LOSSES'].astype(str)
+
+    # Special Case for certain players
+    finaldf.loc[finaldf['PLAYER'] == 'Karl-Anthony Towns', 'COUNTRY'] = 'Dominican Republic'
+    finaldf = finaldf.sort_values('PROB%', ascending=False).reset_index(drop=True)
+
+    # Per Conference
+    eastselection = finaldf[finaldf['CONF'] == 'East'].head(12)
+    westselection = finaldf[finaldf['CONF'] == 'West'].head(12)
+    selectiondf = pd.concat([eastselection,westselection]).reset_index(drop=True)
+
+    worldcolorder = ['PROB%', 'POS', 'PLAYER', 'COUNTRY', 'AGE', 'GP', 'GP%', 'TEAM', 'RANK', 'RECORD',
+                   'MIN', 'PTS', 'REB', 'AST', 'TOV', 'STL', 'BLK']
+
+    worldteam = selectiondf[selectiondf['COUNTRY'] != 'USA'][worldcolorder].sort_values('PROB%', ascending=False).reset_index(drop=True)
+
+    colorder = ['PROB%', 'POS', 'PLAYER', 'AGE', 'GP', 'GP%', 'TEAM', 'RANK', 'RECORD',
+                'MIN', 'PTS', 'REB', 'AST', 'TOV', 'STL', 'BLK']
+
+    usteam1 = selectiondf[selectiondf['COUNTRY'] == 'USA'].sort_values(['WINS', 'PROB%'], ascending=[False, False]).head(8).reset_index(drop=True)
+    usteam2 = selectiondf[selectiondf['COUNTRY'] == 'USA'].sort_values(['WINS', 'PROB%'], ascending=[False, False]).iloc[8:].reset_index(drop=True)
+
+    if len(usteam2) < 8:
+        addusallstars = finaldf[finaldf['COUNTRY'] == 'USA'].iloc[13:13+(8-len(usteam2))]
+        usteam2 = pd.concat([usteam2, addusallstars]).reset_index(drop=True)
+
+    usteam1out = usteam1[colorder].sort_values('PROB%', ascending=False).reset_index(drop=True)
+    usteam2out = usteam2[colorder].sort_values('PROB%', ascending=False).reset_index(drop=True)
+
+    return worldteam, usteam1out, usteam2out
--- nba_award_prediction/predictawardapp.py
+++ nba_award_prediction/predictawardapp.py
@@ -5,11 +5,13 @@ from pathlib import Path
 
 trained_model = Path(__file__).parents[0] / 'models/undersampling_random_forest_v03.pkl'
 trained_allstar_model = Path(__file__).parents[0] / 'models/allstarselection_random_forest_v02.pkl'
+trained_new_allstar_model = Path(__file__).parents[0] / 'models/newformat_allstarselection_random_forest_v01.pkl'
 player_bio_data = Path(__file__).parents[0] / 'data/raw/nba_current_player_bio.csv'
 
 from modules.get_league_standings import get_league_standings
 from modules.get_curr_mvp_table import get_curr_mvp_table
 from modules.get_curr_allstars import get_curr_allstars
+from modules.get_curr_allstars_newformat import get_curr_allstars_newformat
 
 st.set_page_config(
     page_title="NBA Award Predictions",
@@ -19,7 +21,82 @@ st.set_page_config(
 
 st.subheader("2025-26 NBA Regular Season Award Predictions")
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs([":chart_with_upwards_trend: Standings", ":crown: MVP Tracker", ":star: West All-Stars", ":star: East All-Stars", ":earth_africa: International All-Stars"])
+# MVP Predictions
+with open(trained_model, "rb") as model_file:
+    model = pickle.load(model_file)
+
+mvptable = get_curr_mvp_table(model)
+st.session_state["mvptable"] = mvptable
+
+# All-Star Prediction using Old Format
+bio = pd.read_csv(player_bio_data)
+st.session_state["player_bio"] = bio
+
+with open(trained_allstar_model, "rb") as allstar_model_file:
+    allstar_model = pickle.load(allstar_model_file)
+
+weststarters, eaststarters, westbench, eastbench, intallstars = get_curr_allstars(allstar_model, bio)
+st.session_state["weststarters"] = weststarters
+st.session_state["eaststarters"] = eaststarters
+st.session_state["westbench"] = westbench
+st.session_state["eastbench"] = eastbench
+
+@st.fragment()
+def old_format_allstar():
+    left_sel, gap_sel, right_sel, gap_sel2 = st.columns([1.5, 0.5, 1.5, 8.5], vertical_alignment="top")
+    with left_sel:
+        selectconf = st.radio(
+            label="**Conference**",
+            options=["West", "East"],
+            index=0, key='selected_conf'
+        )
+    with right_sel:
+        selecttype = st.radio(
+            label="**Selection Type**",
+            options=["Starters", "Bench"],
+            index=0, key='selected_type'
+        )
+
+    if selectconf == 'West':
+        if selecttype == 'Starters':
+            st.markdown("*with 2 Backcourt(BC) and 3 Frontcourt(FC) players*")
+            st.dataframe(weststarters, hide_index=True)
+        elif selecttype == 'Bench':
+            st.markdown("*with 2 Backcourt(BC), 3 Frontcourt(FC) and 2 Wildcards(WC)*")
+            st.dataframe(westbench, hide_index=True)
+    elif selectconf == 'East':
+        if selecttype == 'Starters':
+            st.markdown("*with 2 Backcourt(BC) and 3 Frontcourt(FC) players*")
+            st.dataframe(eaststarters, hide_index=True)
+        elif selecttype == 'Bench':
+            st.markdown("*with 2 Backcourt(BC), 3 Frontcourt(FC) and 2 Wildcards(WC)*")
+            st.dataframe(eastbench, hide_index=True)
+
+
+# New Format All-Star Predictions
+with open(trained_new_allstar_model, "rb") as model_file:
+    new_allstar_model = pickle.load(model_file)
+
+worldteam, usteam1, usteam2 = get_curr_allstars_newformat(new_allstar_model, bio)
+st.session_state["worldteam"] = worldteam
+st.session_state["usteam1"] = usteam1
+st.session_state["usteam2"] = usteam2
+
+@st.fragment()
+def new_format_allstar():
+    selectteam = st.radio(
+        label="**All-Star Team**",
+        options=["World", "US #1", "US #2"],
+        index=0, key='selected_team', horizontal=True
+    )
+    if selectteam == 'World':
+        st.dataframe(worldteam, hide_index=True)
+    elif selectteam == 'US #1':
+        st.dataframe(usteam1, hide_index=True)
+    elif selectteam == 'US #2':
+        st.dataframe(usteam2, hide_index=True)
+
+tab1, tab2, tab3, tab4 = st.tabs([":chart_with_upwards_trend: Standings", ":crown: MVP Tracker", ":star: Old Format All-Stars", ":earth_africa: U.S. vs. World All-Stars"])
 
 with tab1:
     left_sta, gap_sta, right_sta = st.columns([5.5, 1, 5.5], vertical_alignment="top")
@@ -34,46 +111,25 @@ with tab1:
         st.dataframe(eaststand, hide_index=True)
         st.session_state["eaststand"] = eaststand
 
-
 with tab2:
-    with open(trained_model, "rb") as model_file:
-        model = pickle.load(model_file)
-
-    mvptable = get_curr_mvp_table(model)
     st.dataframe(mvptable, hide_index=True)
     st.session_state["mvptable"] = mvptable
 
 with tab3:
-    bio = pd.read_csv(player_bio_data)
-    st.session_state["player_bio"] = bio
-
-    with open(trained_allstar_model, "rb") as allstar_model_file:
-        allstar_model = pickle.load(allstar_model_file)
-
-    weststarters, eaststarters, westbench, eastbench, intallstars = get_curr_allstars(allstar_model, bio)
-
-    st.markdown(":red-background[Western Conference Starters] *with 2 Backcourt(BC) and 3 Frontcourt(FC) players*")
-    st.dataframe(weststarters, hide_index=True)
-    st.session_state["weststarters"] = weststarters
-
-    st.markdown(":red-background[Western Conference Bench] *with 2 Backcourt(BC), 3 Frontcourt(FC) and 2 Wildcards(WC)*")
-    st.dataframe(westbench, hide_index=True)
-    st.session_state["westbench"] = westbench
+    old_format_allstar()
 
 with tab4:
-    st.markdown(":blue-background[Eastern Conference Starters] *with 2 Backcourt(BC) and 3 Frontcourt(FC) players*")
-    st.dataframe(eaststarters, hide_index=True)
-    st.session_state["eaststarters"] = eaststarters
-
-    st.markdown(":blue-background[Eastern Conference Bench] *with 2 Backcourt(BC), 3 Frontcourt(FC) and 2 Wildcards(WC)*")
-    st.dataframe(eastbench, hide_index=True)
-    st.session_state["eastbench"] = eastbench
-
-with tab5:
-    st.markdown("""This year's All-Star game is expected to feature a round-robin tournament
-                with 2 USA teams and 1 World team consisting of 8 players each.
-                It is not yet clear if player positions or conferences will play a role in the selection.
-                But the following are the current top 10 international players as predicted using the model
-                based on the selection criteria used in the All-Star tabs.""")
-    st.dataframe(intallstars, hide_index=True)
-    st.session_state["intallstars"] = intallstars
+    with st.popover("What is the new All-Star game format ?"):
+        st.markdown("""- This year's All-Star game is featuring two teams of U.S. players and
+                    one team of international players called the World team. There will be
+                    a round-robin tournament with each team having a minimum of eight players.""")
+        st.markdown("""- The voting process remains the same as 12 players from each conference
+                    will be selected, but for the first time without regard to position.""")
+        st.markdown("""- If the voting does not result in the selection of 16 U.S. players
+                    and eight international players, the commissioner will select additional players
+                    to reach the minimum number for either group. In that case,
+                    at least one team would have more than 8 players.""")
+        st.markdown("""- The process for assigning players to the two U.S. teams will be determined at a later date.
+                    But for the purpose of this study, the players are grouped based on team wins so that
+                    U.S. Team #1 features players from teams with higher number of wins.""")
+    new_format_allstar()
